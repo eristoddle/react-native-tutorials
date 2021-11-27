e5fdb4ec3aae02f90e394b3767a0842547c15f04
eristoddle <stephanmil@gmail.com>
2021-11-26 19:51:45 -0600

    Clean Project

--- App.tsx
+++ App.tsx
@@ -1,26 +1,10 @@
 import React, { FC, useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import Dropdown from './src/components/Dropdown';
 
 const App: FC = () => {
-  const [selected, setSelected] = useState(undefined);
-  const data = [
-    { label: 'One', value: '1' },
-    { label: 'Two', value: '2' },
-    { label: 'Three', value: '3' },
-    { label: 'Four', value: '4' },
-    { label: 'Five', value: '5' },
-  ];
-
   return (
     <View style={styles.container}>
-      {!!selected && (
-        <Text>
-          Selected: label = {selected.label} and value = {selected.value}
-        </Text>
-      )}
-      <Dropdown label="Select Item" data={data} onSelect={setSelected} />
-      <Text>This is the rest of the form.</Text>
+      <Text>Blank Slate.</Text>
     </View>
   );
 };
