b1539a4506f1380be9db53a1621590f22656b619
eristoddle <stephanmil@gmail.com>
2023-03-31 19:39:14 -0500

    Remove Expo

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-import 'react-native-gesture-handler';
-import { registerRootComponent } from 'expo';
+/**
+ * @format
+ */
+
+import {AppRegistry} from 'react-native';
+import App from './App';
+import {name as appName} from './app.json';
 import ReactNativeFeatureFlags from 'react-native/Libraries/ReactNative/ReactNativeFeatureFlags';
 
-import App from './App';
-
 ReactNativeFeatureFlags.shouldEmitW3CPointerEvents = () => true;
-
 ReactNativeFeatureFlags.shouldPressibilityUseW3CPointerEventsForHover = () =>
   true;
 
-registerRootComponent(App);
+AppRegistry.registerComponent(appName, () => App);
